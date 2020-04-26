3d77b4d7437dac58cf545525fee2eb39f1889690
Sergio Martínez <sjmp93@gmail.com>
2020-04-26 19:51:49 +0200

    Now, when a user touches the toggle button, if the bluetooth adapter is in off state, it turns on and vice versa. The Bluetooth adapter state changes are recognized through a BroadcastReceiver defined in the viewModel

--- app/src/main/java/com/example/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/view/MainActivity.kt
@@ -1,23 +1,48 @@
 package com.example.myapplication.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.vm.MainViewModel
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
+    private val MY_PERMISSIONS_REQUEST = 0
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
             R.layout.activity_main)
+        viewModel = MainViewModel()
 
-        binding.viewmodel = MainViewModel()
+
+        //binding.btDevice = bluetoothAdapter
+
+        //Set BT initial status according to system status
+        //TODO
+        //if(bluetoothAdapter.isEnabled == binding.viewmodel.data.value!!.binary)
+        //    binding.viewmodel.switchBT()
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN),
+            MY_PERMISSIONS_REQUEST);
+
+        binding.viewmodel = viewModel
         binding.lifecycleOwner = this
+    }
 
-        //setContentView(R.layout.activity_main)
+    override fun onStart() {
+        super.onStart()
+        val bluetoothStatusIntent = Intent(BluetoothAdapter.ACTION_STATE_CHANGED)
+        val intentFilter = IntentFilter(bluetoothStatusIntent.action)
+        //eventReceiver!!.onReceive(this, bluetoothStatusIntent)
+        registerReceiver(viewModel.btEventReceiver, intentFilter)
     }
 }
--- app/src/main/java/com/example/myapplication/vm/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/vm/MainViewModel.kt
@@ -1,20 +1,43 @@
 package com.example.myapplication.vm
 
+import android.bluetooth.BluetoothAdapter
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.myapplication.data.TextData
 
 class MainViewModel : ViewModel(){
     val data: MutableLiveData<TextData>
+    var btDevice: BluetoothAdapter
+    var btEventReceiver : BroadcastReceiver
 
     init{
         data = MutableLiveData()
-        data.value = TextData()
+        btDevice = BluetoothAdapter.getDefaultAdapter()
+        data.value = TextData(binary = btDevice.isEnabled)
+        btEventReceiver = object : BroadcastReceiver(){
+            override fun onReceive(contxt: Context?, intent: Intent?){
+                System.out.println("BT state switched from ${btDevice.isEnabled} to ${!btDevice.isEnabled}")
+                //To avoid triggering switch method twice (during intermediate state and final state) we check State inside intent extra
+                if ((intent!!.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1) == BluetoothAdapter.STATE_ON ||
+                    intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1) == BluetoothAdapter.STATE_OFF) &&
+                    data!!.value!!.binary != btDevice.isEnabled) {
+                    switchBT()
+                }
+            }
+        }
     }
 
-    fun switch(){
+    fun switchBT(){
         var _data = data.value ?: TextData()
         _data.commute()
         data.value = _data
+    }
+
+    fun _switchBT(){
+        switchBT()
+        if(data.value!!.binary) btDevice.enable() else btDevice.disable()
     }
 }
