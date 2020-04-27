a3d06f3b94ace2ddb400bd9074007e51616954a5
Sergio Martínez <sjmp93@gmail.com>
2020-04-28 00:22:53 +0200

    Comments added. No major changes

--- app/src/main/java/com/example/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/view/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     private val btEventReceiver: BroadcastReceiver = object : BroadcastReceiver(){
         override fun onReceive(contxt: Context?, intent: Intent?){
-            System.out.println("BT state switched from ${btDevice.isEnabled} to ${!btDevice.isEnabled}")
+            System.out.println("Switching BT state from ${btDevice.isEnabled} to ${!btDevice.isEnabled}")
             //To avoid triggering switch method twice (during intermediate state and final state) we check State inside intent extra
             if ((intent!!.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1) == BluetoothAdapter.STATE_ON ||
                         intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1) == BluetoothAdapter.STATE_OFF) &&
@@ -39,26 +39,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //Binding with layout file
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
             R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)//MainViewModel()
-        //Bind boolean value with BT device state
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        //Bind boolean value with BT device state (if BT adapter is enabled, then, the app starts with that state for TextData...)
         viewModel.data.value = TextData(binary = btDevice.isEnabled)
+
+        //Observe changes at data to switch btDevice state when data is changed
         viewModel.data.observe(this, Observer<TextData>(){
-             System.out.println("Observer triggered, switching BT state from ${viewModel.data.value!!.binary} to ${!viewModel.data.value!!.binary}")
+             System.out.println("Observer triggered, switching BT state from ${btDevice.isEnabled} to ${!btDevice.isEnabled}")
             if(viewModel.data.value!!.binary) btDevice.enable() else btDevice.disable()
         })
+
+        //Request corresponging permissions for BT functionality
         ActivityCompat.requestPermissions(this,
             arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN),
             MY_PERMISSIONS_REQUEST);
 
+        //Binding ViewModel with view
         binding.viewmodel = viewModel
         binding.lifecycleOwner = this
     }
 
     override fun onStart() {
         super.onStart()
+
+        //Setup for Bluetooth's state changes event receiver
         val bluetoothStatusIntent = Intent(BluetoothAdapter.ACTION_STATE_CHANGED)
         val intentFilter = IntentFilter(bluetoothStatusIntent.action)
         registerReceiver(btEventReceiver, intentFilter)
